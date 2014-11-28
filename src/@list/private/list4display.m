f0d52f80dd98d391dadae4a05e7531d6f319b97f
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-11-28 12:29:00 +0100

    Added header (doc and license).

--- src/@list/private/list4display.m
+++ src/@list/private/list4display.m
@@ -1,5 +1,32 @@
 function str = list4display(o, name, max_number_of_elements)
 
+% Converts a list object to a string.
+%
+% INPUTS 
+% - o                      [list]     A list object.
+% - name                   [string]   Name of the list object (to be displayed).
+% - max_number_of_elements [integer]  Maximum number of elements displayed.
+%
+% OUTPUTS 
+% - str  [string] Representation of the list object as a string. 
+%
+% See also display, disp
+
+% Copyright (C) 2014 Dynare Team
+%
+% This code is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare list submodule is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
 if isempty(o)
     str = sprintf('%s = {}\n', name);
     return
