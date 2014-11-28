63ad5ed51ec5ce1efaa2b19d3f984872bbda1244
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-11-28 12:20:42 +0100

    Moved list4display routine in @list/private.

--- src/@list/private/list4display.m
+++ src/@list/private/list4display.m
@@ -10,14 +10,14 @@ str = sprintf('%s = {', name);
 if length(o)<=max_number_of_elements
     % All the elements are displayed
     for i=1:length(o)-1
-        str = sprintf('%s%s, ', str, o{i});
+        str = sprintf('%s%s, ', str, o.elements{i});
     end
 else
     % Only display four elements (two first and two last)
     for i=1:2
-        str = sprintf('%s%s, ', str, o{i});
+        str = sprintf('%s%s, ', str, o.elements{i});
     end
     str = sprintf('%s%s, ', str, '...');
-    str = sprintf('%s%s, ', str, o{length(o)-1});
+    str = sprintf('%s%s, ', str, o.elements{length(o)-1});
 end
-str = sprintf('%s%s}', str, o{length(o)});
+str = sprintf('%s%s}', str, o.elements{length(o)});
